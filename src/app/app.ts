bd3dfaf7b3482ef50c2aca9e3c71b8600b5c2b41
Weslin-0101 <wesleeylirac12@gmail.com>
2025-07-05 23:19:16 -0300

    feat(app): adding the header component in app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header-component/header-component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
