ad8ac399a9d501d89afd54714eab03ce0cd636c7
Weslin-0101 <wesleeylirac12@gmail.com>
2025-07-06 12:25:07 -0300

    feat(app): created home-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header-component/header-component";
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
