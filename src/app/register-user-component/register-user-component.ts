7ebd375314a9c116f637ac061266ae1c7550dfde
Weslin-0101 <wesleeylirac12@gmail.com>
2025-07-08 11:17:41 -0300

    feat(app): creating the register user component

---
+++ src/app/register-user-component/register-user-component.ts
@@ -0,0 +1,75 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-user-component',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register-user-component.html',
+  styleUrl: './register-user-component.scss'
+})
+export class RegisterUserComponent {
+  @Input() isOpen: boolean = false;
+  @Output() close = new EventEmitter<void>();
+  @Output() loginClick = new EventEmitter<void>();
+
+  nome: string = '';
+  email: string = '';
+  dataNascimento: string = '';
+  cpf: string = '';
+  telefone: string = '';
+  senha: string = '';
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onBackdropClick(event: Event) {
+    if (event.target === event.currentTarget) {
+      this.onClose();
+    }
+  }
+
+  onSubmit() {
+    if (!this.nome || !this.email || !this.dataNascimento || !this.cpf || !this.telefone || !this.senha) {
+      alert('Por favor, preencha todos os campos obrigatórios');
+      return;
+    }
+
+    console.log('Dados do registro:', {
+      nome: this.nome,
+      email: this.email,
+      dataNascimento: this.dataNascimento,
+      cpf: this.cpf,
+      telefone: this.telefone,
+      senha: this.senha
+    });
+  }
+
+  onLoginClick(event: Event) {
+    event.preventDefault();
+    this.loginClick.emit();
+  }
+
+  formatCPF(event: any) {
+    let value = event.target.value.replace(/\D/g, '');
+    if (value.length <= 11) {
+      value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+      this.cpf = value;
+      event.target.value = value;
+    }
+  }
+
+  formatTelefone(event: any) {
+    let value = event.target.value.replace(/\D/g, '');
+    if (value.length <= 11) {
+      if (value.length <= 10) {
+        value = value.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
+      } else {
+        value = value.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+      }
+      this.telefone = value;
+      event.target.value = value;
+    }
+  }
+}
