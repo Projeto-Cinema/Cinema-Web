d33b792a77d89d22a176691c62b448be7facc7a3
Weslin-0101 <wesleeylirac12@gmail.com>
2025-07-09 19:17:27 -0300

    feat(app): integrating the user menu into header-component

--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -1,25 +1,30 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { LoginModal } from "../shared/login-modal/login-modal";
 import { CommonModule } from '@angular/common';
 import { RegisterUserComponent } from "../register-user-component/register-user-component";
 import { AuthService, AuthUser } from '../../service/auth.service';
 import { Subscription } from 'rxjs';
+import { UserMenuComponent } from "../user-menu-component/user-menu-component";
 
 @Component({
   selector: 'app-header-component',
   standalone: true,
-  imports: [LoginModal, CommonModule, RegisterUserComponent],
+  imports: [LoginModal, CommonModule, RegisterUserComponent, UserMenuComponent],
   templateUrl: './header-component.html',
   styleUrl: './header-component.scss'
 })
 export class HeaderComponent implements OnInit, OnDestroy{
   isLoginModalOpen: boolean = false;
   isRegisterModalOpen: boolean = false;
+  isUserMenuOpen: boolean = false;
 
   currentUser: AuthUser | null = null;
   private authSubscription!: Subscription;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private elementRef: ElementRef
+  ) {}
 
   openLoginModal(event: Event) {
     event.preventDefault();
@@ -69,6 +74,18 @@ export class HeaderComponent implements OnInit, OnDestroy{
   switchToLogin() {
     this.isRegisterModalOpen = false;
     this.isLoginModalOpen = true;
+  }
+
+  toggleUserMenu(event: Event) {
+    event.stopPropagation();
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    if (this.isUserMenuOpen && !this.elementRef.nativeElement.querySelector('.user-section').contains(event.target)) {
+      this.isUserMenuOpen = false;
+    }
   }
 
   ngOnInit(): void {
