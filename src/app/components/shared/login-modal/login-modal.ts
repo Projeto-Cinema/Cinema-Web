32798a63f7fb3db7453bd922887e78990c611c9f
Weslin-0101 <wesleeylirac12@gmail.com>
2025-07-08 23:18:07 -0300

    feat(app): integrating the login endpoint in login-modal component

--- src/app/components/shared/login-modal/login-modal.ts
+++ src/app/components/shared/login-modal/login-modal.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormsModule, Validators } from '@angular/forms';
+import { UserLogin, UserService } from '../../../service/user.service';
 
 @Component({
   selector: 'app-login-modal',
@@ -18,7 +19,14 @@ export class LoginModal {
   email: string = '';
   password: string = '';
 
+  isLoading: boolean = false;
+  errorMessage: string = '';
+
+  constructor(private userService: UserService) {}
+
   onClose() {
+    this.errorMessage = '';
+    this.isLoading = false;
     this.close.emit();
   }
 
@@ -29,16 +37,63 @@ export class LoginModal {
   }
 
   onSubmit() {
-    if (this.email && this.password) {
-      console.log('Login attemp:', { email: this.email, password: this.password });
-      this.loginSuccess.emit({ email: this.email, password: this.password });
-      this.email = '';
-      this.password = '';
+    this.errorMessage = '';
+
+    if (!this.email || !this.password) {
+      alert('Por favor, preencha todos os campos obrigatórios');
+      return;
     }
+
+    if (this.password.length < 6) {
+      this.errorMessage = 'A senha deve ter pelo menos 6 caracteres.';
+      return;
+    }
+
+    if (!this.isValidEmail(this.email)) {
+      this.errorMessage = 'Por favor, insira um email válido.';
+      return;
+    }
+
+    const userLogin: UserLogin = {
+      email: this.email.trim().toLowerCase(),
+      senha: this.password
+    };
+
+    console.log(userLogin);
+
+    this.isLoading = true;
+
+    this.userService.loginUser(userLogin).subscribe({
+      next: (response) => {
+        console.log('Login com sucesso:', response);
+
+        localStorage.setItem('access_token', response.access_token);
+        localStorage.setItem('token_type', response.token_type);
+
+        this.loginSuccess.emit(response);
+
+        this.onClose();
+      },
+      error: (error) => {
+        console.error('Erro ao fazer login:', error);
+
+        if (error.status === 401 || error.status === 400) {
+          this.errorMessage = 'Email ou senha inválidos.';
+        } else {
+          this.errorMessage = 'Ocorreu um erro ao fazer login. Por favor, tente novamente mais tarde.';
+        }
+        this.isLoading = false;
+      }
+    })
   }
 
   onRegisterClick(event: Event) {
     event.preventDefault();
     this.registerClick.emit();
+  }
+
+  private isValidEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
 }
