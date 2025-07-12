3c932523424e24cba2f881ed3c9108987161e9c2
Weslin-0101 <wesleeylirac12@gmail.com>
2025-07-11 23:11:20 -0300

    feat(app): implemeting movie search

--- src/app/components/header-component/header-component.ts
+++ src/app/components/header-component/header-component.ts
@@ -5,11 +5,19 @@ import { RegisterUserComponent } from "../register-user-component/register-user-
 import { AuthService, AuthUser } from '../../service/auth.service';
 import { Subscription } from 'rxjs';
 import { UserMenuComponent } from "../user-menu-component/user-menu-component";
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header-component',
   standalone: true,
-  imports: [LoginModal, CommonModule, RegisterUserComponent, UserMenuComponent],
+  imports: [
+    LoginModal,
+    CommonModule,
+    RegisterUserComponent,
+    UserMenuComponent,
+    FormsModule
+  ],
   templateUrl: './header-component.html',
   styleUrl: './header-component.scss'
 })
@@ -17,12 +25,15 @@ export class HeaderComponent implements OnInit, OnDestroy{
   isLoginModalOpen: boolean = false;
   isRegisterModalOpen: boolean = false;
   isUserMenuOpen: boolean = false;
+  isSearchActive: boolean = false;
+  searchQuery: string = '';
 
   currentUser: AuthUser | null = null;
   private authSubscription!: Subscription;
 
   constructor(
     private authService: AuthService,
+    private router: Router,
     private elementRef: ElementRef
   ) {}
 
@@ -96,6 +107,35 @@ export class HeaderComponent implements OnInit, OnDestroy{
   ngOnDestroy(): void {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
+    }
+  }
+
+  openSearch(): void {
+    this.isSearchActive = true;
+  }
+
+  closeSearch(): void {
+    this.isSearchActive = false;
+    this.searchQuery = '';
+  }
+
+  onSearchSubmit(): void {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/search'], { queryParams: { q: this.searchQuery } });
+      this.closeSearch();
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentMovieClick(event: Event): void {
+    const userSection = this.elementRef.nativeElement.querySelector('.user-section');
+    if (this.isUserMenuOpen && userSection && !userSection.contains(event.target)) {
+      this.isUserMenuOpen = false;
+    }
+
+    const searchContainer = this.elementRef.nativeElement.querySelector('.search-container');
+    if (this.isSearchActive && searchContainer && !searchContainer.contains(event.target)) {
+      this.closeSearch();
     }
   }
 }
